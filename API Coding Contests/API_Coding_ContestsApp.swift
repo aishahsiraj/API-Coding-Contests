c265b37e2266cb9cd33b715debe4f0958c028b56
Aishah <aishah122005@gmail.com>
2024-03-13 10:09:42 -0500

    Initial Commit

---
+++ API Coding Contests/API_Coding_ContestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Coding_ContestsApp.swift
+//  API Coding Contests
+//
+//  Created by Aishah Siraj on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct API_Coding_ContestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
